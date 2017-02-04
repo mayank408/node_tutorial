7f7ca8eb9e4c7334f917247d3682ec4d9ee2e7e7
Mayank <mayank.trp48@gmail.com>
2017-02-05 00:41:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var countr = require('./counter')
+
+countr([1,2,3,4,5]);
---
+++ counter.js
@@ -0,0 +1,10 @@
+var counters = function(arry){
+
+	console.log(`the size of array is  ${arry.length}`);
+
+
+}
+
+//counter([1,2,3]);
+
+module.exports = counters;
