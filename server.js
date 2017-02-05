c8f4551a44c5ac6dabe2cfda73e4e3e2afd27994
Mayank <mayank.trp48@gmail.com>
2017-02-06 02:50:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+	console.log('request was made : ' + req.url);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end("hello world");
+
+});
+
+server.listen(3000);
+console.log('Listening to port no 3000');
