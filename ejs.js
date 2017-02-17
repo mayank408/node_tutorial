87d192198331342b72f3c8a4550ac30d75698688
Mayank <mayank.trp48@gmail.com>
2017-02-17 14:05:13 +0530

    ejs module updated

--- ejs.js
+++ ejs.js
@@ -17,7 +17,7 @@ app.get('/contact',function(req , res){
 
 app.get('/profile/:name', function(req,res){
 
-	var data = {age: 29, job:'coder'};
+	var data = {age: 29, job:'coder' , hobbie: ['cycling' , 'fishing' , 'coding']};
 	res.render('profile',{person: req.params.name , data:data})
 
 })
